a3e484cd57d75f775d84f8d4eae4f0fbe2d03ed3
mishgancheg <github@mmak.site>
2025-08-20 20:02:04 +0300

    изменение структуры данных БД

---
+++ DDL/wiki_rag.chunk.sql
@@ -0,0 +1,22 @@
+DROP TABLE IF EXISTS wiki_rag.chunk CASCADE;
+
+CREATE TABLE wiki_rag.chunk
+(
+    chunk_id       SERIAL PRIMARY KEY,
+    wiki_id        TEXT                                               NOT NULL,
+    text           TEXT                                               NOT NULL,
+    embedding_text TEXT                                               NOT NULL,
+    embedding      public.vector(1024)                                NOT NULL,
+    updated_at     TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+COMMENT ON TABLE wiki_rag.chunk IS 'Stores text chunks and their vector embeddings for RAG retrieval system';
+COMMENT ON COLUMN wiki_rag.chunk.chunk_id IS 'Primary key, auto-incrementing identifier for each text chunk record';
+COMMENT ON COLUMN wiki_rag.chunk.wiki_id IS 'Id страницы в WIKI, с которой получен чанк';
+COMMENT ON COLUMN wiki_rag.chunk.text IS 'Original text chunk content (may contain HTML tags or other formatting)';
+COMMENT ON COLUMN wiki_rag.chunk.embedding_text IS 'Processed text used for embedding generation (e.g., HTML stripped version of chunk_text)';
+COMMENT ON COLUMN wiki_rag.chunk.embedding IS 'Vector embedding representation of the embedding_text with dimension 1024';
+COMMENT ON COLUMN wiki_rag.chunk.updated_at IS 'Timestamp indicating when the record was created or last updated';
+
+CREATE INDEX idx_texts_embedding_vector ON wiki_rag.chunk USING ivfflat (embedding vector_cosine_ops);
---
+++ DDL/wiki_rag.question.sql
@@ -0,0 +1,24 @@
+DROP TABLE IF EXISTS wiki_rag.question CASCADE;
+
+CREATE TABLE wiki_rag.question
+(
+    question_id SERIAL PRIMARY KEY,
+    chunk_id    TEXT                                               NOT NULL,
+    wiki_id     TEXT                                               NOT NULL,
+    text        TEXT                                               NOT NULL,
+    embedding   public.vector(1024)                                NOT NULL,
+    updated_at  TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP NOT NULL
+);
+
+
+COMMENT ON TABLE wiki_rag.question IS 'Stores user questions and their vector embeddings for RAG query processing';
+COMMENT ON COLUMN wiki_rag.question.question_id IS 'Primary key, auto-incrementing identifier for each question record';
+COMMENT ON COLUMN wiki_rag.question.chunk_id IS 'chunk_id из таблицы wiki_rag.chunk';
+COMMENT ON COLUMN wiki_rag.question.wiki_id IS 'Id страницы в WIKI, к которой относятся воппросы';
+COMMENT ON COLUMN wiki_rag.question.text IS 'Text content of the user question or query';
+COMMENT ON COLUMN wiki_rag.question.embedding IS 'Vector embedding representation of the question_text with dimension 1024';
+COMMENT ON COLUMN wiki_rag.question.updated_at IS 'Timestamp indicating when the record was created or last updated';
+
+
+
+CREATE INDEX idx_questions_embedding_vector ON wiki_rag.question USING ivfflat (embedding vector_cosine_ops);
